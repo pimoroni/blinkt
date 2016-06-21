8def9212f473f6543cd1178c4a8da0c11e24c20f
Phil Howard <phil@gadgetoid.com>
2016-06-21 15:29:39 +0000

    Tidied up examples, tweak to lib

--- library/blinkt.py
+++ library/blinkt.py
@@ -1,17 +1,31 @@
 import RPi.GPIO as GPIO
+import atexit
 
 DAT = 23
 CLK = 24
 BRIGHTNESS = 7
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup([DAT,CLK],GPIO.OUT)
 
 pixels = [[0,0,0,BRIGHTNESS]] * 8
 
+clear_on_exit = False
+
+def _exit():
+    if clear_on_exit:
+        clear()
+        show()
+    GPIO.cleanup()
+
 def set_brightness(brightness):
     for x in range(NUM_PIXELS):
         pixels[x][3] = int(31.0 * brightness) & 0b11111
+
+def clear():
+    for x in range(NUM_PIXELS):
+        pixels[x][0:3] = [0,0,0]
 
 def write_byte(byte):
     for x in range(8):
@@ -36,3 +50,5 @@ def show():
 def set_pixel(x, r, g, b, brightness=BRIGHTNESS/31.0):
     brightness = int(brightness * 31.0) & 0b11111
     pixels[x] = [r,g,b,brightness]
+
+atexit.register(_exit)
