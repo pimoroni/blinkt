d93cf3bbc2273712d771dffd45e7b90faac2696b
RogueM <chaosgen6@googlemail.com>
2016-07-19 18:01:04 +0100

    using master CHANGELOG

--- packaging/makelog.sh
+++ packaging/makelog.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+mainlog="CHANGELOG"
 debianlog="debian/changelog"
-releaselog="../library/CHANGELOG.txt"
+pypilog="../library/CHANGELOG.txt"
+
+# generate debian changelog
+
+cat $mainlog > $debianlog
+
+# generate pypi changelog
 
 sed -e "/--/d" -e "s/  \*/\*/" \
     -e "s/.*\([0-9].[0-9].[0-9]\).*/\1/" \
     -e '/[0-9].[0-9].[0-9]/ a\
-    -----' $debianlog | cat -s > $releaselog
+    -----' $mainlog | cat -s > $pypilog
 
 exit 0
