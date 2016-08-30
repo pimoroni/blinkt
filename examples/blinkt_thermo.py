2498c2f651d70051c1a53e662e562736dad9a670
AndiCui <andy990525@me.com>
2016-08-30 08:47:17 +0200

    Update blinkt_thermo.py

--- examples/blinkt_thermo.py
+++ examples/blinkt_thermo.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-
 #Data from OpenWeatherMap
 #show_graph function adapted from cpu_temp.py
 
-import blinkt
-import requests
-from time import sleep
 
+import blinkt
+from time import sleep
+try:
+    import requests
+except ImportError:
+    print("This example requires \"requests\" package.")
+    print("Please install it by running \"sudo pip install requests\"")
+    
+    
 #Grab your API key here: http://openweathermap.org
 #List of city ID city.list.json.gz can be downloaded here http://bulk.openweathermap.org/sample/
 API_KEY='a217a2719b9b2640b50dc43ad5e0b468'
@@ -27,7 +32,7 @@ def update_weather():
         temp = r.json().get('main').get('temp')
         return temp
     except:
-        pass
+        print("Connection Error")
 
 def show_graph(v, r, g, b):
     v *= 8
