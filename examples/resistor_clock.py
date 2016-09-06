e686afc49aae580bd2bafef725e6af7b241caac2
Phil Howard <phil@gadgetoid.com>
2016-09-06 14:17:57 +0100

    Refactored set_pixel lines and indent

--- examples/resistor_clock.py
+++ examples/resistor_clock.py
@@ -17,25 +17,37 @@ colours = [
 ]
 
 while True:
-	hour = time.localtime().tm_hour
-	minute = time.localtime().tm_min
+    hour = time.localtime().tm_hour
+    minute = time.localtime().tm_min
 
-	hourten = hour / 10
-	hourunit = hour % 10
-	minuteten = minute / 10
-	minuteunit = minute % 10
+    hourten = hour / 10
+    hourunit = hour % 10
+    minuteten = minute / 10
+    minuteunit = minute % 10
 
-	set_pixel(0,str(colours[hourten][0]),str(colours[hourten][1]),str(colours[hourten][2]))
-	set_pixel(1,str(colours[hourten][0]),str(colours[hourten][1]),str(colours[hourten][2]))
-	set_pixel(2,str(colours[hourunit][0]),str(colours[hourunit][1]),str(colours[hourunit][2]))
-	set_pixel(3,str(colours[hourunit][0]),str(colours[hourunit][1]),str(colours[hourunit][2]))
-	set_pixel(4,str(colours[minuteten][0]),str(colours[minuteten][1]),str(colours[minuteten][2]))
-	set_pixel(5,str(colours[minuteten][0]),str(colours[minuteten][1]),str(colours[minuteten][2]))
-	set_pixel(6,str(colours[minuteunit][0]),str(colours[minuteunit][1]),str(colours[minuteunit][2]))
-	set_pixel(7,str(colours[minuteunit][0]),str(colours[minuteunit][1]),str(colours[minuteunit][2]))
-	show()
-	time.sleep(0.5)
-	set_pixel(7,0,0,0)
-	show()
-	time.sleep(0.5)
+    r, g, b = colours[hourten]
+    set_pixel(0,r,g,b)
+    set_pixel(1,r,g,b)
+
+    r, g, b = colours[hourunit]
+    set_pixel(2,r,g,b)
+    set_pixel(3,r,g,b)
+
+    r, g, b = colours[minuteten]
+    set_pixel(4,r,g,b)
+    set_pixel(5,r,g,b)
+
+    r, g, b = colours[minuteunit]
+    set_pixel(6,r,g,b)
+    set_pixel(7,r,g,b)
+
+    show()
+
+    time.sleep(0.5)
+
+    set_pixel(7,0,0,0)
+
+    show()
+
+    time.sleep(0.5)
 
