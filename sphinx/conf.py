e286f1ef13ceecb4a82eef75984266a3b5c4a65d
Phil Howard <Phil@gadgetoid.com>
2016-11-03 13:00:54 +0000

    Added shop logo to sphinx doc, dropped cruft

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -129,7 +129,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [
@@ -149,7 +152,7 @@ html_theme_path = [
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = 'shop-logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
