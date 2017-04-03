22c6bacdddf5dd5fab7e9894981cf24b525cf3c0
Phil Howard <phil@gadgetoid.com>
2017-04-03 12:28:50 +0000

    Python 3 fix for #49

--- examples/cheerlights.py
+++ examples/cheerlights.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import time
+import struct
 from sys import exit
 
 try:
@@ -8,15 +9,22 @@ try:
 except ImportError:
     exit("This script requires the requests module\nInstall with: sudo pip install requests")
 
-from blinkt import set_clear_on_exit, set_pixel, show
+from blinkt import set_clear_on_exit, set_all, show
 
 set_clear_on_exit()
 
+def hex_to_rgb(col_hex):
+    col_hex = col_hex.lstrip("#")
+    try:
+        return struct.unpack("BBB",bytes.fromhex(col_hex))
+    except AttributeError:
+        return struct.unpack("BBB",col_hex.decode("hex"))
+
 while True:
-    r = requests.get('http://api.thingspeak.com/channels/1417/field/2/last.json', timeout=2)
-    col = r.json()['field2']
-    r, g, b = tuple(ord(c) for c in col[1:].lower().decode('hex'))
-    for i in range(8):
-        set_pixel(i, r, g, b)
+    r = requests.get("http://api.thingspeak.com/channels/1417/field/2/last.json", timeout=2)
+    r, g, b = hex_to_rgb(r.json()["field2"])
+
+    set_all(r, g, b)
+
     show()
-    time.sleep(1)
+    time.sleep(5) # Be friendly to the API
