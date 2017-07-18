c2048e0375588c21f719c60dc1a4e85ef45e7c51
Phil Howard <phil@gadgetoid.com>
2017-07-18 13:36:19 +0100

    Fix larson.py for #56

--- examples/larson.py
+++ examples/larson.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
 import time
+import math
 
 import blinkt
 
 blinkt.set_clear_on_exit()
 
-REDS = [0, 0, 0, 0, 0, 16, 64, 255, 64, 16, 0, 0, 0, 0, 0]
+REDS = [0, 0, 0, 0, 0, 16, 64, 255, 64, 16, 0, 0, 0, 0, 0, 0]
 
 start_time = time.time()
 
 while True:
-    delta = (time.time() - start_time) * 16
-
     # Sine wave, spends a little longer at min/max
-    # offset = int(round(((math.sin(delta) + 1) / 2) * 7))
+    # delta = (time.time() - start_time) * 8
+    # offset = int(round(((math.sin(delta) + 1) / 2) * (blinkt.NUM_PIXELS - 1)))
 
     # Triangle wave, a snappy ping-pong effect
-    offset = int(abs((delta % 16) - 8))
+    delta = (time.time() - start_time) * 16
+    offset = int(abs((delta % len(REDS)) - blinkt.NUM_PIXELS))
 
-    for i in range(8):
+    for i in range(blinkt.NUM_PIXELS):
         blinkt.set_pixel(i , REDS[offset + i], 0, 0)
 
     blinkt.show()
