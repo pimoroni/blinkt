e65452e133618d291be164b48b4c0859c4239536
druck <RPi-F@druck.org.uk>
2020-03-09 15:28:07 +0000

    Tidyup, show moved out of inner loop, delay increased

--- examples/unionjack.py
+++ examples/unionjack.py
@@ -1,41 +1,31 @@
 #!/usr/bin/env python
 
-import colorsys
 import time
 import blinkt
 
-Red   = (255,0,0)
-White = (127,127,127)
-Blue  = (0,0,255)
+RED   = (255,0,0)
+WHITE = (127,127,127)
+BLUE  = (0,0,255)
 
-Pattern = \
+pattern = \
 [
-    [   Red,    Blue,   White,  Red,    Red,    White,  Blue,   Red,    ],
-    [   Blue,   Red,    White,  Red,    Red,    White,  Red,    Blue,   ],
-    [   White,  White,  White,  Red,    Red,    White,  White,  White,  ],
-    [   Red,    Red,    Red,    Red,    Red,    Red,    Red,    Red,    ],
-    [   Red,    Red,    Red,    Red,    Red,    Red,    Red,    Red,    ],
-    [   White,  White,  White,  Red,    Red,    White,  White,  White,  ],
-    [   Blue,   Red,    White,  Red,    Red,    White,  Red,    Blue,   ],
-    [   Red,    Blue,   White,  Red,    Red,    White,  Blue,   Red,    ],
+    [   RED,    BLUE,   WHITE,  RED,    RED,    WHITE,  BLUE,   RED,    ],
+    [   BLUE,   RED,    WHITE,  RED,    RED,    WHITE,  RED,    BLUE,   ],
+    [   WHITE,  WHITE,  WHITE,  RED,    RED,    WHITE,  WHITE,  WHITE,  ],
+    [   RED,    RED,    RED,    RED,    RED,    RED,    RED,    RED,    ],
+    [   RED,    RED,    RED,    RED,    RED,    RED,    RED,    RED,    ],
+    [   WHITE,  WHITE,  WHITE,  RED,    RED,    WHITE,  WHITE,  WHITE,  ],
+    [   BLUE,   RED,    WHITE,  RED,    RED,    WHITE,  RED,    BLUE,   ],
+    [   RED,    BLUE,   WHITE,  RED,    RED,    WHITE,  BLUE,   RED,    ],
 ]
 
 blinkt.set_clear_on_exit()
 blinkt.set_brightness(0.1)
 
-blinkt.set_pixel(0,   0,   0, 255)
-blinkt.set_pixel(1,   0,   0, 255)
-blinkt.set_pixel(2, 195, 195, 195)
-blinkt.set_pixel(3, 255,   0,   0)
-blinkt.set_pixel(4, 255,   0,   0)
-blinkt.set_pixel(5, 195, 195, 195)
-blinkt.set_pixel(6,   0,   0, 255)
-blinkt.set_pixel(7,   0,   0, 255)
-blinkt.show()
-
 while 1:
-    for line in Pattern:
-        for i in range(len(line)):
-            blinkt.set_pixel(i, line[i][0], line[i][1], line[i][2])
-            blinkt.show()
-        time.sleep(0.2)
+    for line in pattern:
+        for i,pixel in enumerate(line):
+            r, g, b = pixel
+            blinkt.set_pixel(i, r, g, b)
+        blinkt.show()
+        time.sleep(0.25)
