6c78a58c39b800b59d7be958a725d5f3376e2a48
druck <RPi-F@druck.org.uk>
2020-03-09 15:29:16 +0000

    Arg check corrected, arg decode simplified, brightness default to None

--- examples/setall.py
+++ examples/setall.py
@@ -4,14 +4,14 @@ import sys
 from sys import argv
 import blinkt
 
-if len(argv)<2 or len(argv)>5:
-    sys.stderr.write( "Syntax: {0} <red> <green> <blue> [brightness]]\n".format(argv[0]) )
+if len(argv) < 4 or len(argv) > 5:
+    sys.stderr.write("Syntax: {0} <red> <green> <blue> [brightness]\n".format(argv[0]))
     exit(1)
 
-red    = int(argv[1])   if len(argv)>3 else 0
-green  = int(argv[2])   if len(argv)>3 else 0
-blue   = int(argv[3])   if len(argv)>3 else 0
-bright = float(argv[4]) if len(argv)>4 else 0.0
+red    = int(argv[1])
+green  = int(argv[2])
+blue   = int(argv[3])
+bright = float(argv[4]) if len(argv) > 4 else None
 
 blinkt.set_clear_on_exit(False)
 blinkt.set_all(red, green, blue, bright)
