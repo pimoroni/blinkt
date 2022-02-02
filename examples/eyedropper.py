a4600dce4a37f8e928af744926030183b6c0d14e
Phil Howard <phil@gadgetoid.com>
2022-02-02 10:38:55 +0000

    Lint examples

--- examples/eyedropper.py
+++ examples/eyedropper.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from PIL import Image, ImageGrab
-import pyautogui, sys
-import blinkt 
+from PIL import ImageGrab
+import pyautogui
+import blinkt
 import time
 
 print('Press Ctrl-C to quit.')
@@ -10,10 +10,10 @@ print('Press Ctrl-C to quit.')
 try:
     while True:
         x, y = pyautogui.position()
-        im = ImageGrab.grab(bbox =(x-1, y, x, y+1))
+        im = ImageGrab.grab(bbox=(x - 1, y, x, y + 1))
         rawrgb = list(im.getdata())
-        rgb = str(rawrgb)[2:-2]   
-        r, g, b =  rgb.split(', ')  
+        rgb = str(rawrgb)[2:-2]
+        r, g, b = rgb.split(', ')
         blinkt.set_all(r, g, b)
         blinkt.set_brightness(1)
         blinkt.show()
