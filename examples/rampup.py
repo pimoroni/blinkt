3ce1d0584b3e7193d4ea13451c2c778c8e6826b3
druck <RPi-Z@druck.org.uk>
2022-06-27 00:07:19 +0100

    Added brightness parameter

--- examples/rampup.py
+++ examples/rampup.py
@@ -4,7 +4,7 @@ from time import sleep
 import blinkt
 
 for i in range(256):
-    blinkt.set_all(i, i, i)
+    blinkt.set_all(i, i, i, 1.0)
     sys.stdout.write("%3d" % i)
     blinkt.show()
     sleep(0.1)
